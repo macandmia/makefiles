04f8913588ea35d98c5d767edf75d75a65d5bf7c
Eric Marden <eric.marden@gmail.com>
2017-10-17 16:36:05 -0500

    tweaks and documentation

--- golang.mk
+++ golang.mk
@@ -1,12 +1,35 @@
+# Include this in the Makefile of projects written in
+# golang that use glide for dependency management.
+#
+#    include github.com/macandmia/makefiles/golang
+#
+# PLEASE NOTE: you must set PKG_NAME var in your project's Makefile.
+
 GLIDE := $(shell command -v glide 2> /dev/null)
 GOVERALLS := $(shell command -v goveralls 2> /dev/null)
 GOLINT := $(shell command -v golint 2> /dev/null)
 GOMETALINT := $(shell command -v gometalinter 2> /dev/null)
 
-.PHONY: gotools install deps test godocs
+.PHONY: install deps test docs golang_tools golang_check
 
-# install go tools
-gotools:
+# install dependencies
+install:
+	glide install
+
+# update dependencies
+deps:
+	glide up
+
+# run tests
+test:
+	go test $(glide nv)
+
+# read the docs
+docs: golang_check
+	@echo "RTFM: http://localhost:6060/pkg/${PKG_NAME}/"
+	godoc -http=:6060
+
+golang_tools:
 ifndef GLIDE
 	go get -u github.com/Masterminds/glide
 else
@@ -29,18 +52,8 @@ else
 	@echo "Already Installed: goveralls. Skipping."
 endif
 
-# install dependencies
-install:
-	glide install
-
-# update dependencies
-deps:
-	glide up
-
-# run tests
-test:
-	go test $(glide nv)
-
-# start documentation server
-godocs:
-	godoc -http=:6060
+golang_check:
+ifndef PKG_NAME
+    	@echo "PKG_NAME must be set. Aborting."
+    	@exit 1
+endif
