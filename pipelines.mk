e515e0c654abbe4cb306ddd3bfea151c6a5eefbf
Eric Marden <eric.marden@gmail.com>
2017-10-17 16:02:39 -0500

    adds heroku remotes and checks for required vars before running commands

--- pipelines.mk
+++ pipelines.mk
@@ -2,29 +2,41 @@
 #
 #    include github.com/macandmia/makefiles/pipelines
 #
-# PLEASE NOTE: you must set PROJECT_DIR and STAGING_APP vars in your project's Makefile.
-#
-# `make staging` assumes you have a git remote named staging.
+# PLEASE NOTE: you must set PROJECT_DIR, STAGING_APP, PRODUCTION_APP vars in your project's Makefile.
+
+.PHONY: staging production remotes preflight pipelines_check
 
 # deploy app to staging
 staging: preflight
 	git push staging
 
 # deploy app to production
-production:
-ifdef ${STAGING_APP}
-    	@echo "STAGING_APP must be set. Aborting deploy."
-else
+production: pipelines_check
 	heroku pipelines:promote -a ${STAGING_APP}
-endif
 
-preflight:
-ifndef ${PROJECT_DIR}
-	@echo "Ensuring your working copy is up to date with master before pushing..."
+# add heroku remotes
+remotes: pipelines_check
+	heroku git:remote -a ${STAGING_APP} -r staging
+	heroku git:remote -a ${PRODUCTION_APP} -r production
+
+# preflight checks if your working copy is up-to-date with origin master
+preflight: pipelines_check
 	git -C ${PROJECT_DIR} fetch origin
 	git -C ${PROJECT_DIR} status | grep 'On branch master'
 	git -C ${PROJECT_DIR} status | grep "Your branch is up-to-date with 'origin/master'."
 	git -C ${PROJECT_DIR} status | grep 'nothing to commit, working tree clean'
-else
-	@echo "PROJECT_DIR must be set. Aborting deploy."
+endif
+
+pipelines_check:
+ifndef PROJECT_DIR
+	@echo "PROJECT_DIR must be set. Aborting."
+	@exit 1
+endif
+ifndef STAGING_APP
+    	@echo "STAGING_APP must be set. Aborting."
+    	@exit 1
+endif
+ifndef PRODUCTION_APP
+    	@echo "PRODUCTION_APP must be set. Aborting."
+    	@exit 1
 endif
