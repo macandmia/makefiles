fa785c4e45c8adcec8d3a9dc52753a5274edd367
Eric Marden <eric.marden@gmail.com>
2017-11-14 13:23:59 -0600

    adds assets.mk

---
+++ assets.mk
@@ -0,0 +1,31 @@
+.PHONY: install-sassc
+PKG_INSTALLER := "brew install"
+PKG_UPGRADER := "brew upgrade"
+SASSC := $(shell command -v sassc 2> /dev/null)
+OPTIPNG := $(shell command -v optipng 2> /dev/null)
+PNG2ICO := $(shell command -v png2ico 2> /dev/null)
+JPEGTRAN := $(shell command -v jpegtran 2> /dev/null)
+CONVERT := $(shell command -v convert 2> /dev/null)
+
+define install_pkg
+ifndef $(1)
+	${PKG_INSTALLER} $(2)
+else
+	${PKG_UPGRADER} $(2)
+endif
+enddef
+
+install-sassc:
+	$(call install_pkg, SASSC, sassc)
+
+install-optipng:
+	$(call install_pkg, OPTIPNG, optipng)
+
+install-png2ico:
+	$(call install_pkg, PNG2ICO, png2ico)
+
+install-jpegtran:
+	$(call install_pkg, JPEGTRAN, libjpeg)
+
+install-convert:
+	$(call install_pkg, CONVERT, imagemagick)
