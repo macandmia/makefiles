df08022b63039467df0155181544880ca274a75c
Eric Marden <eric.marden@gmail.com>
2018-06-04 21:44:23 -0500

    adds install-pipenv

--- tools.mk
+++ tools.mk
@@ -22,10 +22,14 @@ define install_pkg
 	$(if $(1), @ echo "Already Installed: ${2}", $(PKG_INSTALLER) $(2))
 endef
 
+define add_apt_repo
+	sudo add-apt-repository -y ppa:${}/ppa
+	sudo apt update
+endef
 
 install-jq:
 	$(call install_pkg, $(JQ), jq)
 
 install-pipenv:
-	$(if $(filter ${UNAME}, Linux), echo "Running Linux", echo "Running Mac")
+	$(if $(filter ${UNAME}, Linux), $(call add_apt_repo pypa), @ true)
 	$(call install_pkg, $(PIPENV), pipenv)
